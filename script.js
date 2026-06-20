12301458905c149369406a787ec0d999a8f59ff5
jjaimeosori-netizen <jjaimeosori@gmail.com>
2026-06-20 04:08:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -84,72 +84,70 @@ document.addEventListener("DOMContentLoaded", () => {
     // INVITADOS
     // ==========================
     const invitados = {
-        "ABUE Y PA": 2,
-        "FLIA ESPELETA QUINTANA": 4,
-        "FLIA JAIME ESPELETA": 3,
-        "TIA LILIA": 1,
-        "TIA CARMEN": 10,
-        "TIA SONIA": 1,
-        "TIA YINA": 1,
-        "YANDRI, MEMI, BIAN": 2,
-        "DANEIBIS": 1,
-        "ROGER Y ANGIE": 2,
-        "EDILBERTO": 1,
-        "ROBERTO Y ROCIO": 2,
-        "MARIA TERESA": 1,
-        "JUAN Y AMALIA": 2,
-        "FELIX Y ADRIANA": 2,
-        "GERALDINE": 1,
-        "FLIA AHUMADA ESPITIA": 4,
-        "JUAN Y MA OLGA": 2,
-        "FLIA SANCHEZ MONTEVERDE": 4,
-        "JORGE Y MARILYN": 2,
-        "WILFER": 1,
-        "SUSANA Y MARIANA": 2,
-        "CARLOS ARTURO": 1,
-        "JESSICA, REY Y LUISA": 3,
-        "ANA ISABEL": 1,
-        "JULIETA": 1,
-        "SOFIA GARAVITO": 1,
-        "SARA ANILLO": 1,
-        "SARA MAZABUEL": 1,
-        "LUISA": 1,
-        "MARIANA LAMBIS": 1,
-        "MARIANA CERRO": 1,
-        "JHOSUA": 1,
-        "JUAN CAMILO": 1,
-        "ISABELLA Y ELIZABETH": 2,
-        "SAUL": 1,
-        "THIAGO": 1,
-        "MA ISABEL": 1,
-        "VALENTINA": 1,
-        "ANA LUISA": 1,
-        "SHARI": 1,
-        "EVELYN": 1,
-        "ESTEBAN": 1,
-        "MAIC": 1,
-        "MARIANGEL": 1,
-        "FLIA DE LA CRUZ MOLINA": 4,
-        "FLIA BALLESTAS BAENA": 4,
-        "DANNA": 1,
-        "ASHLY": 1,
-        "VALERY": 1,
-        "NATALIA": 1,
-        "ISABEL": 1,
-        "SOFIA": 1,
-        "FELIPE": 1
-    };
+    "ABUE Y PA": 2,
+    "FLIA ESPELETA QUINTANA": 4,
+    "FLIA JAIME ESPELETA": 3,
+    "TIA LILIA": 1,
+    "TIA CARMEN": 10,
+    "TIA SONIA": 1,
+    "TIA YINA": 1,
+    "YANDRI, MEMI, BIAN": 2,
+    "DANEIBIS": 1,
+    "ROGER Y ANGIE": 2,
+    "EDILBERTO": 1,
+    "ROBERTO Y ROCIO": 2,
+    "MARIA TERESA": 1,
+    "JUAN Y AMALIA": 2,
+    "FELIX Y ADRIANA": 2,
+    "GERALDINE": 1,
+    "FLIA AHUMADA ESPITIA": 4,
+    "JUAN Y MA OLGA": 2,
+    "FLIA SANCHEZ MONTEVERDE": 4,
+    "JORGE Y MARILYN": 2,
+    "WILFER": 1,
+    "SUSANA Y MARIANA": 2,
+    "CARLOS ARTURO": 1,
+    "JESSICA, REY Y LUISA": 3,
+    "ANA ISABEL": 1,
+    "JULIETA": 1,
+    "SOFIA GARAVITO": 1,
+    "SARA ANILLO": 1,
+    "SARA MAZABUEL": 1,
+    "LUISA": 1,
+    "MARIANA LAMBIS": 1,
+    "MARIANA CERRO": 1,
+    "JHOSUA": 1,
+    "JUAN CAMILO": 1,
+    "ISABELLA Y ELIZABETH": 2,
+    "SAUL": 1,
+    "THIAGO": 1,
+    "MA ISABEL": 1,
+    "VALENTINA": 1,
+    "ANA LUISA": 1,
+    "SHARI": 1,
+    "EVELYN": 1,
+    "ESTEBAN": 1,
+    "MAIC": 1,
+    "MARIANGEL": 1,
+    "FLIA DE LA CRUZ MOLINA": 4,
+    "FLIA BALLESTAS BAENA": 4,
+    "DANNA": 1,
+    "ASHLY": 1,
+    "VALERY": 1,
+    "NATALIA": 1,
+    "ISABEL": 1,
+    "SOFIA": 1,
+    "FELIPE": 1
+};
 
-    const parametros = new URLSearchParams(window.location.search);
+const parametros = new URLSearchParams(window.location.search);
 
-    const nombre = parametros.get("invitado") || "INVITADO ESPECIAL";
+const nombre = parametros.get("invitado") || "INVITADO ESPECIAL";
 
-    const cupos = invitados[nombre] || 1;
+const cupos = invitados[nombre] || 1;
 
-    const nombreEl = document.getElementById("nombreInvitado");
-    const cuposEl = document.getElementById("cuposInvitado");
+document.getElementById("nombreInvitado").innerHTML =
+    nombre.toUpperCase();
 
-    if (nombreEl) nombreEl.innerHTML = nombre.toUpperCase();
-    if (cuposEl) cuposEl.innerHTML = `${cupos} PERSONA(S)`;
-
-});
+document.getElementById("cuposInvitado").innerHTML =
+    `${cupos} PERSONA(S)`;
